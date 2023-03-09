393d3095b3c703ae7aec84feadac666168617d79
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-03-09 16:15:54 +0300

    Задача 2.

---
+++ Exercise001_MaxMin/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число 1-ое: ");
+double n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2-ое: ");
+double n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2)
+{
+    Console.WriteLine($"Большее: {n1} ; "  + "Меньшее:" + n2);
+}
+else {
+    Console.WriteLine($"Большее: {n2} ; " + "Меньшее: " + n1);
+}
