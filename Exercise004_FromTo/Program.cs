1818c7ddc84996067666643619d8db36ee0bbddf
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-03-09 18:12:37 +0300

    Задача 8

---
+++ Exercise004_FromTo/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите конец промежутка: ");
+int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите начало промежутка: ");
+// int i = Convert.ToInt32(Console.ReadLine());
+
+// while (i <= num)
+// {
+//     if (i % 2 != 1)
+//     {
+//         Console.Write(i + ", ");
+        
+//     }
+//      i++; 
+// }
+
+
+for (int i = 1; i  <= num;  i++){
+    if (i % 2 != 1){
+        Console.Write($"{i} ;");
+    }
+}   
