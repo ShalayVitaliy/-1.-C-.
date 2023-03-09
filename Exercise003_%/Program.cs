9de7247cf3342f7cde9b10d932bfda336fd312eb
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-03-09 17:43:53 +0300

    Задача 6

---
+++ Exercise003_%/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if ((num % 2) == 0) 
+{
+    Console.WriteLine("Число чётное");
+}
+else {
+    Console.WriteLine("Число не чётное");
+}
